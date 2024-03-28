c4740685d040bc3a5ef2a8373bcb2364b4f6f246
Nikhil <its.nikhil2808@gmail.com>
2024-03-28 15:56:41 +0530

    adeed

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 
+const githubdata={
+    "name":"aman",
+    "age":"18",
+    "school":"nit"
+}
 app.get('/', (req, res) => {
   res.send('Hello Worlddddddd!')
 })
 app.get('/a', (req, res) => {
-    res.send('Hello a!!!!!!!')
+    res.send('Hello worlss')
+  })
+  app.get('/data', (req, res) => {
+    res.json(githubdata)
   })
 
 app.listen(port, () => {
